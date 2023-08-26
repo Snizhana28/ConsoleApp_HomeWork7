114bcbbbad209b804acc705e257e40e7f3736650
snizhana28 <snizhana2812kostyuk@gmail.com>
2023-08-26 14:59:08 +0300

    Done Task2

---
+++ Domain/Method.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_HomeWork7.Domain
+{
+    public class Method
+    {
+        public static void ShowTime()
+        {
+            Console.WriteLine(DateTime.Now.ToString("HH:mm:ss"));
+        }
+        public static void ShowDate()
+        {
+            Console.WriteLine(DateTime.Now.ToString("dd.MM.yyyy"));
+        }
+        public static void ShowDayOfWeek()
+        {
+            Console.WriteLine(DateTime.Now.ToString("dddd"));
+        }
+        public static void ShowTriangleArea(double a, double b, double c)
+        {
+            double p = (a + b + c) / 2;
+            double s = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            Console.WriteLine($"Площа трикутника = {s}");
+        }
+        public static void ShowRectangleArea(double a, double b)
+        {
+            double s = a * b;
+            Console.WriteLine($"Площа прямокутника = {s}");
+        }
+
+    }
+}
---
+++ Internal/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ConsoleApp_HomeWork7.Domain;
+namespace ConsoleApp_HomeWork7.Internal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Action action = Method.ShowTime;
+            action += Method.ShowDate;
+            action += Method.ShowDayOfWeek;
+            action();
+            Console.WriteLine();
+
+            Action<double, double, double> func = Method.ShowTriangleArea;
+            func(3, 4, 5);
+
+            Action<double, double> func2 = Method.ShowRectangleArea;
+            func2(3, 4);
+        }
+    }
+}
