956c631c6725c3b89e600b03a5bbb4227c0cec2c
snizhana28 <snizhana2812kostyuk@gmail.com>
2023-08-26 14:34:50 +0300

    Done Task1

---
+++ Domain/MathFunc.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_HomeWork7.Domain
+{
+    public class MathFunc
+    {
+        public static void IsEven(int[] array)
+        {
+            Console.WriteLine("\nEven numbers:");
+            foreach (var item in array)
+            {
+                if (item % 2 == 0)
+                {
+                    Console.Write(item + " ");
+                }
+            }
+        }
+        public static void IsOdd(int[] array)
+        {
+            Console.WriteLine("\nOdd numbers:");
+            foreach (var item in array)
+            {
+                if (item % 2 != 0)
+                {
+                    Console.Write(item + " ");
+                }
+            }
+        }
+
+        public static void IsPrime(int[] array)
+        {
+            Console.WriteLine("\nPrime numbers:");
+            foreach (var item in array)
+            {
+                if (item == 1)
+                {
+                    continue;
+                }
+                bool isPrime = true;
+                for (int i = 2; i < item; i++)
+                {
+                    if (item % i == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    Console.Write(item + " ");
+                }
+            }
+        }
+        public static void IsFibonacci(int[] array)
+        {
+            Console.WriteLine("\nFibonacci numbers:");
+            foreach (var item in array)
+            {
+                int a = 0;
+                int b = 1;
+                int c = 0;
+                while (c < item)
+                {
+                    c = a + b;
+                    a = b;
+                    b = c;
+                }
+                if (c == item)
+                {
+                    Console.Write(item + " ");
+                }
+            }
+        }
+    }
+}
---
+++ Internal/Program.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleApp_HomeWork7.Domain;
+namespace ConsoleApp_HomeWork7.Internal
+{
+    internal class Program
+    {
+        delegate void MyDelegate(int[] array);
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8 };
+            MathFunc mathFunc = new MathFunc();
+            MyDelegate myDelegate = MathFunc.IsEven;
+            myDelegate += MathFunc.IsOdd;
+            myDelegate += MathFunc.IsPrime;
+            myDelegate += MathFunc.IsFibonacci;
+            myDelegate(array);
+            Console.WriteLine();
+        }
+    }
+}
